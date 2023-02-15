38f9062e184b2023a13cf7b2885a93615830c7f5
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2023-02-15 11:26:37 +0100

    Update BlogPost.java

--- src/main/java/paizo/crawler/BlogPost.java
+++ src/main/java/paizo/crawler/BlogPost.java
@@ -32,6 +32,6 @@ public class BlogPost {
 
 	@JsonIgnore
 	public boolean checked() {
-		return date!=null && date.toLocalDate().isBefore(LocalDate.of(2020,7,1));
+		return date!=null && date.toLocalDate().isBefore(LocalDate.of(2021,2,1));
 	}
 }

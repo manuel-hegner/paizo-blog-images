c50a07fce5ab2c71cbd01b639472b3005c8ae61a
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2026-05-08 18:05:52 +0200

    Add mapping so that new images are refreshed

--- src/main/java/paizo/crawler/StoreImages.java
+++ src/main/java/paizo/crawler/StoreImages.java
@@ -4,10 +4,11 @@ import java.io.File;
 import java.net.URI;
 import java.nio.file.Files;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.TreeMap;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.jsoup.Jsoup;
 
@@ -42,48 +43,69 @@ public class StoreImages {
 		double averageScore
 	) {}
 	
+	private static record ImageMappings(TreeMap<String, ImageMapping> map) {}
+	private static record ImageMapping(String sku, String url, String optimizedImage) {}
+	
 	public static void main(String[] args) throws Exception {
 		var wikiInfo = Jackson.JSON.readValue(
 			URI.create("https://pathfinderwiki.com/wiki/Template:Paizo_store/data?action=raw").toURL(),
 			WikInfo.class
 		);
-		
+		File cfg = new File("data/store_image_mapping.yaml");
+		var mappings = Jackson.MAPPER.readValue(cfg, ImageMappings.class);
 		File out = new File("data/store_images");
 		out.mkdirs();
-		var existingSkus = new HashSet<>();
-		Arrays.asList(out.listFiles()).forEach(f->{
-			if(f.isFile()) {
-				String name = f.getName();
-				name = name.substring(0, name.lastIndexOf("."));
-				existingSkus.add(name);
-			}
-		});
 		
 		MyPool pool = new MyPool("StoreImages");
-		for(var productInfo:wikiInfo.values().values()) {
-			pool.submit(()-> {
-				var product = productInfo.properties;
-				try {
-					if(product.storeImage == null || existingSkus.contains(product.sku)) return null;
-					
-					byte[] bytes = Jsoup.connect(product.storeImage).maxBodySize(0).ignoreContentType(true).execute().bodyAsBytes();
-					if(bytes.length<10 || Bytes.indexOf(bytes, ImageDownloader.HTML_INDICATOR)!=-1) {
-						log.error("{} results in an illegal image file", product.sku);
-						return null;
+		var results = wikiInfo.values().values()
+			.stream()
+			.map(productInfo->
+				pool.submit(()-> {
+					var product = productInfo.properties;
+					try {
+						if(product.storeImage == null) return null;
+						var oldMapping = mappings.map.get(product.sku);
+						if(oldMapping != null && oldMapping.url.equals(product.storeImage)) return oldMapping;
+						
+						byte[] bytes = Jsoup.connect(product.storeImage).maxBodySize(0).ignoreContentType(true).execute().bodyAsBytes();
+						if(bytes.length<10 || Bytes.indexOf(bytes, ImageDownloader.HTML_INDICATOR)!=-1) {
+							log.error("{} results in an illegal image file", product.sku);
+							return null;
+						}
+						String inputExtension = FilenameUtils.getExtension(product.storeImage);
+						var tmp = Files.createTempFile("", "."+inputExtension);
+						Files.write(tmp, bytes);
+						var optimized = ImageOptimizer.optimize(tmp.toFile());
+						
+						var imgFile = new File(out, product.sku+"."+optimized.optimizedExtension());
+						FileUtils.deleteQuietly(imgFile);
+						Files.move(optimized.result().toPath(), imgFile.toPath());
+						return new ImageMapping(product.sku, product.storeImage, imgFile.getName());
+					} catch(Exception e) {
+						throw new RuntimeException("Failed for " + product.sku, e);
 					}
-					String inputExtension = FilenameUtils.getExtension(product.storeImage);
-					var tmp = Files.createTempFile("", "."+inputExtension);
-					Files.write(tmp, bytes);
-					var optimized = ImageOptimizer.optimize(tmp.toFile());
-					
-					var imgFile = new File(out, product.sku+"."+optimized.optimizedExtension());
-					Files.move(optimized.result().toPath(), imgFile.toPath());
-					return null;
-				} catch(Exception e) {
-					throw new RuntimeException("Failed for " + product.sku, e);
-				}
-			});
-		}
+				})
+			).toList();
 		pool.shutdown();
+		
+		results.forEach(f-> {
+			try {
+				var r = f.get();
+				if(r != null)
+					mappings.map.put(r.sku, r);
+			} catch (InterruptedException | ExecutionException e) {
+				throw new RuntimeException(e);
+			}
+			
+		});
+		
+		Jackson.MAPPER.writeValue(cfg, mappings);
+		
+		for(var f:out.listFiles()) {
+			if(mappings.map.values().stream().noneMatch(m->Objects.equals(m.optimizedImage, f.getName()))) {
+				log.info("Deleting outdated {}", f.getName());
+				FileUtils.deleteQuietly(f);
+			}
+		}
 	}
 }
